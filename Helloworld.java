741758a5f60c9a9acacfff916e129e5bc0776dea
11maomao25 <165105407+11maomao25@users.noreply.github.com>
2024-03-30 23:00:15 +0800

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,12 @@
+package d1;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		 System.out.println("hello£¬world!");  
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
